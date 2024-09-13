6a2f2237a70443222c74192230a496db3d6ddfbe
PholSoophak <sokphakphol@gmail.com>
2024-09-13 09:32:46 +0700

    new Test Casde

--- Test Suites/TestSuit_cmd_Jenkins.ts
+++ Test Suites/TestSuit_cmd_Jenkins.ts
@@ -12,16 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>f2af1f0a-e181-4e04-931b-79a4f2348b85</testSuiteGuid>
    <testCaseLink>
-      <guid>caeb9398-c4e3-4f0a-95ca-cf1e7e674942</guid>
+      <guid>a9ba6b37-4aa5-4ab1-81bf-ab96641c2dc0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Automates-test-demo/Script-Demo</testCaseId>
+      <testCaseId>Test Cases/Jenkins_TestCase</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>0f37b2b6-4d88-4825-ba52-bce3a6eea9fc</variableId>
-      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
